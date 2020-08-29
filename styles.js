90ea58f706d66a3917e28fce383a99f4a7ba28de
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-08-29 14:50:12 -0300

    finished

---
+++ styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;  
+`
+
+export const Buttons = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const ButtonContainer = styled.View`
+  margin: 10px 5px;  
+`
+
+export const List = styled.View`
+  justify-content: center;
+  align-items: center;
+`
